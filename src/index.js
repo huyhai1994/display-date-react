51132fc269893824253b1a3d27fdf4617f5550dc
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 05:14:51 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+/*TODO: Khai báo biến root và khởi tạo với ReactDOM.createRoot(), sau đó truyền element vừa tạo vào hàm root.render() để render element ra U*/
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const tick = () => {
+    root.render(<div>
+        {/*TODO:Khởi tạo element mang giá trị hiển thị thời gian hiện tại
+*/}
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>);
+};
+/*TODO: Buớc 4: Sử dụng hàm hàm tick() để cập nhật thời gian*/
+setInterval(tick, 1000);
